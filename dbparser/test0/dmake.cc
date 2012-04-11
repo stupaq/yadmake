a422e3abe416d867eabb4ce82256d6a1661120cf
Waclaw Banasik <wb305160@students.mimuw.edu.pl>
2012-04-11 20:44:33 +0200

    merge, test0 works

--- dbparser/test0/dmake.cc
+++ dbparser/test0/dmake.cc
@@ -6,10 +6,9 @@
 int main(){
    std::vector< RemoteWorker *> free_workers;
    free_workers.push_back(0);
-   DependencyGraph dep_graph(0);
+   DependencyGraph dep_graph(dup(0));
    std::vector<std::string> basics;
    basics.push_back("make");
    count_commands(&dep_graph,basics, "blah");
-   std::cout << "dispatcher" << std::endl;
    Dispatcher(dep_graph, free_workers);
 }
