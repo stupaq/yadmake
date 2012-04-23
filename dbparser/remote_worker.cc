07184530f48e4d7e6a62e9d0923d794192d98282
MartaDrozdek <martadrozdek0@gmail.com>
2012-04-23 20:00:56 +0300

    Update dbparser/remote_worker.cc

--- dbparser/remote_worker.cc
+++ dbparser/remote_worker.cc
@@ -1,116 +1,47 @@
-#include <sys/types.h>
-#include <stdlib.h>
-#include <vector>
-#include <map>
 #include <string>
-#include <boost/foreach.hpp>
-#include <stdio.h>
+#include <istream>
+#include <stdlib.h>
+#include <stdio.h> 
+#include <stdexcept>
 #include <libssh/libssh.h>
-#include "err.h"
-#include "remote_worker.h"
+
+#ifndef _REMOTEWORKER_
+#define _REMOTEWORKER_
+
+class NoConnection : public std::runtime_error {
+public:
+NoConnection() : std::runtime_error("connection  failed") {}
+};
 
 
-void RemoteWorker::close_connection() {
-  ssh_disconnect(session);
-  ssh_free(session);
-}
+class RemoteWorker{
+  public:
+    const char * name;
+    
+    RemoteWorker(const char * name, char * password);
 
-void RemoteWorker::connect_to() {
-  int rc;
+//run commands to create a target
+    void realize(std::vector<std::string> commands, std::string path);
+
+//creates ssh connection
+    void connect_to();
+
+    void close_connection();
+
+  private:
+    char * password;
+    ssh_session session;
+    int port;
+    ssh_channel channel;
+    
+    int open_channel();
+//run a single remote command    
+    int run_command(char * command);
+    
+// read data displayed by remote command   
+    int read_results();
+
   
-  session = ssh_new();
-  if (session == NULL)
-    syserr("ssh_new");
+};
 
-  ssh_options_set(session, SSH_OPTIONS_HOST, name);
-  ssh_options_set(session, SSH_OPTIONS_PORT, &port);
-  ssh_options_set(session, SSH_OPTIONS_USER, username);
-
-  
-  rc = ssh_connect(session);
-  if (rc != SSH_OK)
-  {
-    ssh_free(session);
-    throw NoConnection(name);
-  }
-  
-  rc = ssh_userauth_password(session, NULL, password);
-  if (rc != SSH_AUTH_SUCCESS)
-  {
-    syserr("Error authenticating with password");
-    close_connection();
-  }
-
-}
-
-RemoteWorker::RemoteWorker(std::string name) {
-    this.name = name;
-    port = 22;
-}
-
-
-int RemoteWorker::open_channel() {
-  channel = ssh_channel_new(session);
-  if (channel == NULL)
-    return SSH_ERROR;
-
-  rc = ssh_channel_open_session(channel);
-  if (rc != SSH_OK) {
-    ssh_channel_free(channel);
-    return rc;
-  }
-  return 0;
-}
-
-
-int RemoteWorker::run_command(std::string command) {
-  rc = ssh_channel_request_exec(channel, command);
-  if (rc != SSH_OK)
-  {
-    ssh_channel_close(channel);
-    ssh_channel_free(channel);
-    return rc;
-  }
-  return 0;
-}
-
-
-int RemoteWorker::read_results() {
-  char buffer[256];
-  unsigned int nbytes;
-
-  nbytes = ssh_channel_read(channel, buffer, sizeof(buffer), 0);
-  while (nbytes > 0)
-  {
-    if (fwrite(buffer, 1, nbytes, stdout) != nbytes)
-    {
-      ssh_channel_close(channel);
-      ssh_channel_free(channel);
-      return SSH_ERROR;
-    }
-    nbytes = ssh_channel_read(channel, buffer, sizeof(buffer), 0);
-  }
-
-  if (nbytes < 0)
-  {
-    ssh_channel_close(channel);
-    ssh_channel_free(channel);
-    return SSH_ERROR;
-  }
-  return 0;
-}
-
-void RemoteWorker::realize(std::vector<std::string> commands) {
-  open_channel();
-  
-  BOOST_FOREACH(std::string s, commands_) {
-    (void) run_command(s);
-    (void) read_results();
-  }
-  
-  ssh_channel_send_eof(channel);
-  ssh_channel_close(channel);
-  ssh_channel_free(channel);
-  
-}
-
+#endif
