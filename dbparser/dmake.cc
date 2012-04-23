5614f619e7a3a4cc0f7e16a025fe27f175972e80
MartaDrozdek <martadrozdek0@gmail.com>
2012-04-23 21:51:00 +0300

    using RemoteWorker

--- dbparser/dmake.cc
+++ dbparser/dmake.cc
@@ -1,9 +1,18 @@
-#include "dispatcher.h"
 #include <vector>
+#include <boost/foreach.hpp>
+#include "dispatcher.h"
+#include "remote_worker.h"
 
 int main(){
-   std::vector< RemoteWorker *> free_workers;
-   free_workers.push_back(0);
-   DependencyGraph dep_graph(0);
-   Dispatcher(dep_graph, free_workers);
+  char* user = "md292600";
+  char* password = getpass("");
+  std::vector< RemoteWorker *> free_workers;
+  free_workers.push_back(new RemoteWorker("yellow01", password, user));
+  free_workers.push_back(new RemoteWorker("yellow02", password, user));
+  BOOST_FOREACH(RemoteWorker* rw, free_workers)
+    rw->connect_to();
+  DependencyGraph dep_graph(0);
+  Dispatcher(dep_graph, free_workers);
+  BOOST_FOREACH(RemoteWorker* rw, free_workers)
+    rw->close_connection();
 }
