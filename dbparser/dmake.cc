f71e4b1b1deea8991e9c481ee46e8c88bc5af5a8
MartaDrozdek <martadrozdek0@gmail.com>
2012-04-23 21:53:55 +0300

    using RemoteWorker

--- dbparser/dmake.cc
+++ dbparser/dmake.cc
@@ -5,10 +5,10 @@
 
 int main(){
   char* user = "md292600";
-  char* password = getpass("");
+  char* password = getpass("Password: ");
   std::vector< RemoteWorker *> free_workers;
-  free_workers.push_back(new RemoteWorker("yellow01", password, user));
-  free_workers.push_back(new RemoteWorker("yellow02", password, user));
+  free_workers.push_back(new RemoteWorker("brown01", password, user));
+  free_workers.push_back(new RemoteWorker("brown02", password, user));
   BOOST_FOREACH(RemoteWorker* rw, free_workers)
     rw->connect_to();
   DependencyGraph dep_graph(0);
