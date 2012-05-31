57339821e7781e0854381c4600603e923d9cf0f5
Waclaw Banasik <wb305160@students.mimuw.edu.pl>
2012-06-01 00:22:47 +0200

    ReinitInord added

--- include/dispatcher.h
+++ include/dispatcher.h
@@ -11,12 +11,11 @@
  * @param dependency_graph - constructed dep. graph with, all targets
  * with commands set.
  * @param free_workres - vector of workers that will be used to bulid targets
- * @param messaging - nie mam pomysłu na ten moment TODO
  * @param keep_going - is -k option set - if yes, continue after error
  * requires:
- * proper inord in dependency graph
  * dependency_graph all targets with commands
  * workers ready to build
+ * (calls ReinitInord)
  * state after:
  * inord NOT correct (TODO ?)
  * workers are free
