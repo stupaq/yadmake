47520509af63d075c0c9387d6a55675c8eb2166e
Mateusz Machalica <mateuszmachalica@gmail.com>
2012-06-04 22:24:12 +0200

    Testing main added, but dispatcher executes every signle target...

---
+++ yadmake.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+
+#include "dbparser.h"
+#include "exec.h"
+#include "worker.h"
+#include "dispatcher.h"
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	const string make_command = "make";
+	vector<string> args;
+	args.push_back("-pq");
+	if (argc > 1) {
+		args.push_back("-C");
+		args.push_back(argv[1]);
+	}
+	pair<string, string> out = exec(make_command, args);
+
+	/* create graph */
+	DependencyGraph graph(out.first);
+
+	/* get commands */
+	const string delimiter = "3344543508980989031231";
+	vector<string> basics;
+	graph.CountCommands(basics, delimiter);
+
+	/* dump makefile */
+	graph.DumpMakefile(cout);
+
+	/* run dispatcher */
+	Dispatcher(graph, false);
+
+	return 0;
+}
