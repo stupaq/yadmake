403a4707e4371aa599c007b57111e18678d8a315
Waclaw Banasik <wb305160@students.mimuw.edu.pl>
2012-06-09 21:35:07 +0200

    signal handling in yadmake, by Ania

--- yadmake.cc
+++ yadmake.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <boost/foreach.hpp>
+#include <signal.h>
 
 #include "dbparser.h"
 #include "exec.h"
@@ -8,11 +9,37 @@
 
 using namespace std;
 
+vector<Worker *> workers;
+Messaging *m = NULL;
+
+void clean_all() {
+	rename("DMakefile", "Makefile");
+	BOOST_FOREACH(Worker *w, workers)
+		delete w;
+	if (m != NULL)
+		delete m;
+}
+
+void handler(int sig_number) {
+	clean_all();
+	exit(sig_number);
+}
+
+void catch_signals() {
+	struct sigaction action;
+	action.sa_handler = handler;
+	action.sa_flags = 0;
+	sigaction(SIGINT, &action, NULL);
+	sigaction(SIGTERM, &action, NULL);
+}
+
 int main(int argc, char* argv[]) {
+
+	/* clean everything after a signal */
+	catch_signals();
+
 	const string make_command = "make";
 	vector<string> args;
-  vector<Worker *> workers;
-  Messaging *m;
 
 	args.push_back("-pq");
 	if (argc > 1) {
@@ -32,18 +59,18 @@ int main(int argc, char* argv[]) {
 	/* dump makefile */
 	graph.DumpMakefile(cout);
 
-  /* create messaging, workers, not ready to use */
-  m = new Messaging();
-  workers = get_workers(m);
+	/* create messaging, workers, not ready to use */
+	m = new Messaging();
+	workers = get_workers(m);
 
 	/* run dispatcher */
 	Dispatcher(graph, workers, m, false);
 
-  /* delete workers, messaging */
-  BOOST_FOREACH(Worker *w, workers)
-    delete w;
+	/* delete workers, messaging */
+	BOOST_FOREACH(Worker *w, workers)
+		delete w;
 
-  delete m;
+	delete m;
 
 	return 0;
 }
